4ab5b350211cd2e51e171935356ad8d7ac0fe3f9
Abikrishna2004 <dharineeshdharineesh54@gmail.com>
2026-02-18 13:39:46 +0530

    change the backend routes code

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,17 +1,21 @@
 from fastapi import FastAPI
-from routes import router
 from fastapi.middleware.cors import CORSMiddleware
 
+from routes import courses, trainers, placements, startups, certificates
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"], 
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-app.include_router(router, prefix="/api", tags=["Project API"])
-
-
+# Include all routers
+app.include_router(courses.router, prefix="/api")
+app.include_router(trainers.router, prefix="/api")
+app.include_router(placements.router, prefix="/api")
+app.include_router(startups.router, prefix="/api")
+app.include_router(certificates.router, prefix="/api")
